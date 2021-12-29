65d7fb3ed5e1a1f32450a73c9d741f8a146ee8eb
soffi49 <zosia.wrona49@gmail.com>
2021-12-29 23:32:26 +0100

    Added Booking classes

---
+++ backend/src/main/java/com/parkly/backend/repo/BookingHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.parkly.backend.repo;
+
+import com.parkly.backend.repo.domain.BookingHistoryDTO;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingHistoryRepository extends PagingAndSortingRepository<BookingHistoryDTO, Long> {
+}
