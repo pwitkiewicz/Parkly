c0e061182bc71b9165cf4995e06358b1563196d3
Michał Derej <contact@michalderej.com>
2021-12-29 13:42:47 +0100

    Add header component

---
+++ frontend/parkly/src/constants/Styles.tsx
@@ -0,0 +1,19 @@
+const palette = {
+    grey: '#5E6073',
+    blue: '#10316B',
+    green: '#0B8457',
+    yellow: '#EAC100',
+    red: '#FF5F5F'
+}
+
+const Theme = {
+    colors: {
+        header: palette.grey,
+        button: palette.blue,
+        accept: palette.green,
+        edit: palette.yellow,
+        remove: palette.red
+    }
+}
+
+export default Theme;
