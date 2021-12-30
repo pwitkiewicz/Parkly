9482774ebd29f8bcb5477a397d7b005a09731efd
Kamil Jakoniuk <tsunderekurisu@Kamils-MacBook-Pro.local>
2021-12-30 20:40:08 +0100

    add edit modal

--- frontend/parkly/src/components/parkingSpotItem/ParkingSpotItem.tsx
+++ frontend/parkly/src/components/parkingSpotItem/ParkingSpotItem.tsx
@@ -1,27 +1,36 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import styled from "@emotion/styled";
 import {Button, Card, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
 
 import {ParkingSpot} from "../../models/models";
 import Theme from "../../constants/Styles";
+import EditParkingSpotModal from '../../pages/parkingSpotsPage/components/EditParkingSpotModal';
 
-const ParkingSpotItem: FC<ParkingSpot> = ({name, description, location, cost, photos}) => {
+type ParkingSpotEditModal = {
+    isVisible: boolean;
+}
+
+const ParkingSpotItem: FC<ParkingSpot> = (parkingPlace) => {
+   
+    const [editParkingSpotState, setParkingSpotState] = useState<ParkingSpotEditModal>({
+        isVisible: false
+    });
     return (
         <StyledCard>
-            <CardMedia component="img" image={photos[0]?.path} alt="Parking spot image"/>
+            <CardMedia component="img" image={parkingPlace.photos[0]?.path} alt="Parking spot image"/>
             <CardContent>
                 <Typography variant="h4">
-                    {name}
+                    {parkingPlace.name}
                 </Typography>
                 <TypographyContainer>
                     <Typography variant="body1" style={{marginTop: '1vh'}}>
-                        {description}
+                        {parkingPlace.description}
                     </Typography>
                     <Typography variant="body1" style={{marginTop: '1vh', fontWeight: 'bold'}}>
-                        {location.city}
+                        {parkingPlace.location.city}
                     </Typography>
                     <Typography variant="body1" style={{marginTop: '1vh', fontWeight: 'bold'}}>
-                        {cost} PLN
+                        {parkingPlace.cost} PLN
                     </Typography>
                 </TypographyContainer>
             </CardContent>
@@ -29,17 +38,24 @@ const ParkingSpotItem: FC<ParkingSpot> = ({name, description, location, cost, ph
                 <Button style={{color: `${Theme.colors.accept}`, marginRight: '1.5vw'}}>
                     Book
                 </Button>
-                <Button style={{color: `${Theme.colors.edit}`, marginRight: '1.5vw'}}>
+                <Button onClick={() => {
+                    setParkingSpotState({isVisible: true})
+                }} style={{color: `${Theme.colors.edit}`, marginRight: '1.5vw'}}>
                     Edit
                 </Button>
                 <Button style={{color: `${Theme.colors.remove}`}}>
                     Delete
                 </Button>
+                <EditParkingSpotModal visible={editParkingSpotState.isVisible} onCancel={() =>
+                    setParkingSpotState({
+                        isVisible: false,
+                    })} parkingPlace={parkingPlace}
+                    
+                />
             </CardActions>
         </StyledCard>
     )
 }
-
 const StyledCard = styled(Card)`
   max-width: 320px;
   margin: 30px;
---
+++ frontend/parkly/src/pages/parkingSpotsPage/components/EditParkingSpotModal.tsx
@@ -0,0 +1,199 @@
+import React, {ChangeEventHandler, useCallback, useState} from 'react';
+import {Moment} from "moment/moment";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControlLabel,
+    Switch,
+    TextField
+} from "@mui/material";
+import DateAdapter from '@mui/lab/AdapterMoment';
+import {DesktopDatePicker, LocalizationProvider} from "@mui/lab";
+import styled from "@emotion/styled";
+import {addParkingSpot} from "../../../queries/queries";
+import { ParkingSpot } from '../../../models/models';
+
+interface Props {
+    visible: boolean;
+    onCancel: () => void;
+    parkingPlace: ParkingSpot;
+}
+
+/* TODO: Add a way to upload photos and enter location
+*   Then modify their handlers to properly save data
+*   At the end send data to BE with proper call */
+
+// TODO 2: Fix date pickers warnings
+
+const EditParkingSpotModal: React.FC<Props> = (props) => {
+
+    const [editedParkingModal, setEditedParkingModal] = useState({
+        id: props.parkingPlace.id,
+        name: props.parkingPlace.name,
+        startDateTime: props.parkingPlace.startDateTime,
+        endDateTime: props.parkingPlace.endDateTime,
+        isActive: props.parkingPlace.isActive,
+        isDisabledFriendly: props.parkingPlace.isDisabledFriendly,
+        photos: props.parkingPlace.photos,
+        description: props.parkingPlace.description,
+        height: props.parkingPlace.height,
+        width: props.parkingPlace.width,
+        location: props.parkingPlace.location,
+        cost: props.parkingPlace.cost
+    })
+
+    const handleChangeName: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, name: event.target.value}));
+    }
+    const handleChangeStartDate = (date: Moment | null) => {
+        const startDate = date?.toDate() || new Date();
+        setEditedParkingModal(prev => ({...prev, startDateTime: startDate}));
+    }
+    const handleChangeEndDate = (date: Moment | null) => {
+        const endDate = date?.toDate() || new Date();
+        setEditedParkingModal(prev => ({...prev, endDateTime: endDate}));
+    }
+    const handleChangeIsActive: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, isActive: event.target.checked}));
+    }
+    const handleChangeIsDisabledFriendly: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, isDisabledFriendly: event.target.checked}));
+    }
+    const handleChangePhotos: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev}));
+    }
+    const handleChangeDescription: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, description: event.target.value}));
+    }
+    const handleChangeHeight: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, height: Number(event.target.value)}));
+    }
+    const handleChangeWidth: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, width: Number(event.target.value)}));
+    }
+    const handleChangeLocation: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, id: event.target.value}));
+    }
+    const handleChangeCost: ChangeEventHandler<HTMLInputElement> = event => {
+        setEditedParkingModal(prev => ({...prev, cost: Number(event.target.value)}));
+    }
+
+    const handleSubmit = useCallback(() => {
+        addParkingSpot(editedParkingModal);
+    }, []);
+
+    return (
+        <Dialog open={props.visible} onClose={props.onCancel}>
+            <DialogTitle>Edit parking spot details</DialogTitle>
+            <DialogContent>
+                <TextField
+                    margin="dense"
+                    id="name"
+                    label="Name"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleChangeName}
+                    value={editedParkingModal.name}
+                />
+                <LocalizationProvider dateAdapter={DateAdapter}>
+                    <DatePickerContainer>
+                        <DatePickerWrapper>
+                            <DesktopDatePicker
+                                label="Start Date"
+                                inputFormat="DD/MM/yyyy"
+                                value={editedParkingModal.startDateTime}
+                                onChange={handleChangeStartDate}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </DatePickerWrapper>
+                        <DesktopDatePicker
+                            label="End Date"
+                            inputFormat="DD/MM/yyyy"
+                            value={editedParkingModal.endDateTime}
+                            onChange={handleChangeEndDate}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </DatePickerContainer>
+                </LocalizationProvider>
+                <CheckBoxesContainer>
+                    <FormControlLabel control={<Switch onChange={handleChangeIsActive}/>} label="Active"
+                                      checked={editedParkingModal.isActive}/>
+                    <FormControlLabel control={<Switch onChange={handleChangeIsDisabledFriendly}/>}
+                                      label="Disabled friendly"
+                                      checked={editedParkingModal.isDisabledFriendly}/>
+                </CheckBoxesContainer>
+                <TextField
+                    margin="dense"
+                    id="description"
+                    label="Description"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    multiline
+                    rows={4}
+                    onChange={handleChangeDescription}
+                    value={editedParkingModal.description}
+                />
+                <TextField
+                    inputProps={{inputMode: 'numeric', pattern: '[0-9]*'}}
+                    margin="dense"
+                    id="length"
+                    label="Length"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleChangeHeight}
+                    value={editedParkingModal.height}
+                />
+                <TextField
+                    inputProps={{inputMode: 'numeric', pattern: '[0-9]*'}}
+                    margin="dense"
+                    id="width"
+                    label="Width"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleChangeWidth}
+                    value={editedParkingModal.width}
+                />
+                <TextField
+                    inputProps={{inputMode: 'numeric', pattern: '[0-9]*'}}
+                    margin="dense"
+                    id="cost"
+                    label="Cost"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleChangeCost}
+                    value={editedParkingModal.cost}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={props.onCancel}>Cancel</Button>
+                <Button onClick={handleSubmit}>Edit</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+const DatePickerContainer = styled.div`
+  margin-top: 4vh;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const DatePickerWrapper = styled.div`
+  margin-right: 2vw;
+`
+
+const CheckBoxesContainer = styled.div`
+  margin-top: 3vh;
+  display: flex;
+  flex-direction: column;
+`
+
+export default EditParkingSpotModal;
