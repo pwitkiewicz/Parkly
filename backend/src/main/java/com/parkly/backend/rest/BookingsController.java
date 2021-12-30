c2bda2b50a60483fe1a5f96bf770e541e786bd69
Piotr Witkiewicz <piotr.witkiewicz.stud@pw.edu.pl>
2021-12-30 23:17:16 +0100

    [PAR-32] intial commit

--- backend/src/main/java/com/parkly/backend/rest/BookingsController.java
+++ backend/src/main/java/com/parkly/backend/rest/BookingsController.java
@@ -1,9 +1,20 @@
 package com.parkly.backend.rest;
 
+import com.parkly.backend.bizz.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/bookings")
 public class BookingsController {
+
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingsController(final BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+
 }
