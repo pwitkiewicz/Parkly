713863874fb658f37930d74e45f31030017d09a1
Michał Derej <contact@michalderej.com>
2022-01-05 20:07:13 +0100

    [PAR-8] Add React Router with sample pages

---
+++ frontend/parkly/src/pages/notFoundPage/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header/Header";
+import React from "react";
+
+const ParkingSpotsPage = () => {
+    return (
+        <>
+            <Header/>
+            Page not found 😭😭😭
+        </>
+    )
+}
+
+export default ParkingSpotsPage;
