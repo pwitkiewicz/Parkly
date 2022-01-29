5a7128bd63aad4b73a13a7c7d287f1f2c002835e
Michał Derej <contact@michalderej.com>
2022-01-29 18:37:29 +0100

    [PAR-14] Add logging service

--- frontend/parkly/src/pages/notAuthorizedPage/NotAuthorizedPage.tsx
+++ frontend/parkly/src/pages/notAuthorizedPage/NotAuthorizedPage.tsx
@@ -1,13 +1,67 @@
 import Header from "../../components/header/Header";
-import React from "react";
+import React, {ChangeEventHandler, useState} from "react";
+import styled from "@emotion/styled";
+import {Box, Button, TextField, Typography} from "@mui/material";
+import AuthService from "../../services/AuthService";
+import {LoginInformation} from "../../models/models";
+import {useNavigate} from "react-router-dom";
 
 const NotAuthorizedPage = () => {
+
+    let navigate = useNavigate();
+    const [loginInfo, setLoginInfo] = useState<LoginInformation>({
+        login: '',
+        password: ''
+    });
+    const handleChangeLogin: ChangeEventHandler<HTMLInputElement> = event => {
+        setLoginInfo(prev => ({...prev, login: event.target.value}));
+    }
+    const handleChangePassword: ChangeEventHandler<HTMLInputElement> = event => {
+        setLoginInfo(prev => ({...prev, password: event.target.value}));
+    }
+
     return (
         <>
             <Header/>
-            You are not authorized. Please login before proceeding.
+            <StyledBox>
+                <StyledTypography>
+                    You are not authorized. Please login before proceeding.
+                </StyledTypography>
+                <StyledTextField id="standard-basic" label="Username" style={{ marginBottom: '15px' }} onChange={handleChangeLogin}/>
+                <FormContainer>
+                    <StyledTextField id="standard-basic" label="Password" type="password" onChange={handleChangePassword}/>
+                    <StyledButton variant="outlined" onClick={async () => {
+                        await AuthService.login(loginInfo);
+                        navigate('/');
+                    }}>Login</StyledButton>
+                </FormContainer>
+            </StyledBox>
         </>
     )
 }
+
+const StyledBox = styled(Box)`
+    width: 100%;
+    max-width: 500px;
+    margin: 50px;
+`;
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px;
+`
+
+const StyledTextField = styled(TextField)`
+    font-size: 12px;
+    min-width: 300px;
+    margin-right: 30px;
+`
+
+const StyledButton = styled(Button)`
+    min-width: 100px;
+`
+
+const FormContainer = styled.div`
+    display: flex;
+`;
 
 export default NotAuthorizedPage;
