16deb94424b94f5d3adc1d07327f8845e27830e9
Piotr Witkiewicz <piotr.witkiewicz.stud@pw.edu.pl>
2022-01-30 17:51:44 +0100

    time utils

--- backend/src/main/java/com/parkly/backend/utils/TimeUtils.java
+++ backend/src/main/java/com/parkly/backend/utils/TimeUtils.java
@@ -2,15 +2,45 @@ package com.parkly.backend.utils;
 
 import lombok.experimental.UtilityClass;
 
-import java.time.Duration;
-import java.time.Instant;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.TextStyle;
+import java.util.Date;
+import java.util.Locale;
 
 @UtilityClass
 public class TimeUtils {
-    public static Duration timestampsToDuration(Long startTime, Long endTime)
+    private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .appendPattern("E MMM d uuuu H:m:s")
+            .appendLiteral(" ")
+            .appendZoneId()
+            .appendPattern("X")
+            .appendLiteral(" ")
+            .appendLiteral("(")
+            .appendGenericZoneText(TextStyle.FULL)
+            .appendLiteral(')')
+            .toFormatter(Locale.ENGLISH);
+
+    public static Duration timestampsToDuration(final Long startTime, final Long endTime)
     {
         final Instant startInstant = Instant.ofEpochSecond(startTime);
         final Instant endInstant = Instant.ofEpochSecond(endTime);
         return Duration.between(startInstant, endInstant);
+    }
+
+    public static Long stringToUnixTimestamp(final String dateTimeString) {
+        final OffsetDateTime dateTime = OffsetDateTime.parse(dateTimeString, formatter);
+        return dateTime.atZoneSameInstant(ZoneOffset.UTC).toInstant().getEpochSecond();
+    }
+
+    public static String unixTimestampToString(final Long timestamp) {
+        final OffsetDateTime dateTime = OffsetDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneOffset.UTC);
+
+        return dateTime.format(formatter).toString();
     }
 }
---
+++ backend/src/test/java/com/parkly/backend/utils/TimeUtilsTest.java
@@ -0,0 +1,24 @@
+package com.parkly.backend.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TimeUtilsTest {
+
+    @Test
+    void convertCorrectlyStringToUnixTimestamp() {
+        var localDateTimeString = "Sun Jan 30 2022 15:12:45 GMT+0100 (CET)";
+        long timestamp = TimeUtils.stringToUnixTimestamp(localDateTimeString);
+
+        assertThat(timestamp).isEqualTo(1643551965L);
+    }
+
+    @Test
+    void convertCorrectlyTimestampToString() {
+        var timestamp = 1643551965L;
+        var localDateTimeString = TimeUtils.unixTimestampToString(timestamp);
+
+        assertThat(localDateTimeString).isEqualTo("Sun Jan 30 2022 15:12:45 GMT+0100 (CET)");
+    }
+}
